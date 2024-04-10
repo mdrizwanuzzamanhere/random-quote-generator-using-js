c5a69d7001b5b8083e769fc55b0672ac5db6816b
mdrizwanuzzamanhere <123739101+mdrizwanuzzamanhere@users.noreply.github.com>
2024-04-10 10:21:14 +0530

    Add files via upload

---
+++ Random Quote Generator/script.js
@@ -0,0 +1,11 @@
+let btn = document.getElementById('btn');
+
+let result = document.getElementById('result');
+
+let quote = [`"If people knew how hard I worked to get my mastery, it wouldn't seem so wonderful after all." — Michelangelo`, `"Do the best you can until you know better. Then when you know better, do better." — Maya Angelou`, `"Success is no accident. It is hard work, perseverance, learning, studying, sacrifice and most of all, love of what you are doing or learning to do." — Pele`, `"I do not know anyone who has got to the top without hard work. That is the recipe. It will not always get you to the top, but should get you pretty near." — Margaret Thatcher`, `"Hard work spotlights the character of people: some turn up their sleeves, some turn up their noses, and some dont turn up at all." — Sam Ewing`, `"The only thing standing between you and outrageous success is continuous progress you need discipline." — Dan Waldschmidt`, `"Talent is cheaper than table salt. What separates the talented individual from the successful one is a lot of hard work."— Stephen King`, `"Hard work is a prison sentence only if it does not have meaning. Once it does, it becomes the kind of thing that makes you grab your wife around the waist and dance a jig." — Malcolm Gladwell`, `"The fight is won or lost far away from witnesses — behind the lines, in the gym, and out there on the road, long before I dance under those lights." — Muhammad Ali`, `"Most of the important things in the world have been accomplished by people who have kept on trying when there seemed to be no hope at all." — Dale Carnegie`, `"The only place where success comes before work is in the dictionary." — Vince Lombardi`, `"Strive not to be a success, but rather to be of value." — Albert Einstein`, `“If you want to live a happy life, tie it to a goal, not to people or things.” ― Albert Einstein
+`];
+
+btn.addEventListener('click', function () {
+    var randomQuote = quote[Math.floor(Math.random() * quote.length)];
+    result.innerHTML = randomQuote;
+})
